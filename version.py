2d7470294b45f42b81ba7107b1838bf8cbdb4279
fernandobritosc <136645278+fernandobritosc@users.noreply.github.com>
2026-01-16 12:53:16 -0300

    Create version.py

---
+++ version.py
@@ -0,0 +1,8 @@
+VERSION = "12.0.1"
+STATUS = "Operacional - Correção de API e Data"
+LAST_UPDATE = "16/01/2026"
+CHANGELOG = [
+    "Fix: ModuleNotFoundError resolvido",
+    "Fix: APIError na inserção de matérias corrigido",
+    "Fix: Data Prova forçada para DD/MM/YYYY no UI"
+]
