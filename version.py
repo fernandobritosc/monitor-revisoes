eef3c7ed4b45dc56b059d230399fb56ca75362a5
fernandobritosc <136645278+fernandobritosc@users.noreply.github.com>
2026-01-16 13:06:29 -0300

    Update version.py

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
-# version.py - Controle de Versão Oficial
+# version.py - Identidade do Sistema
 VERSION = "13.0.1"
-STATUS = "Operacional - Correção de API e Datas"
+STATUS = "Operacional - Correção de Banco e Datas"
 LAST_UPDATE = "16/01/2026"
 CHANGELOG = [
     "Resolvido erro de importação (ModuleNotFoundError)",
-    "Corrigido erro de inserção de matéria (APIError)",
-    "Blindagem de visualização de datas (DD/MM/YYYY)"
+    "Corrigido erro de inserção no banco (APIError)",
+    "Trava visual definitiva para datas DD/MM/YYYY"
 ]
