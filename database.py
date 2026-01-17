1986c9ae5cb2a7801800d12ee669280e2b42a401
fernandobritosc <136645278+fernandobritosc@users.noreply.github.com>
2026-01-16 22:15:45 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from supabase import create_client, Client
 
 @st.cache_resource
 def init_connection():
-    # Puxa as chaves de segurança do Streamlit Secrets
+    # Puxa as credenciais que já estão no seu Streamlit Secrets
     return create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
 
 supabase = init_connection()
